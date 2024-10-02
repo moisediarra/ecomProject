dfd6e14d5adf871b2831a003fb3787a8524f5767
Moise DIARRA <moisepr@gmail.com>
2024-10-02 23:46:20 +0000

    add index.html in app folder

--- ecomProject/settings.py
+++ ecomProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ecomProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'app/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
