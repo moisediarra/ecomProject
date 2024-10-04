75dd7c5bf997ada6bf6334e774cc359504cb5df6
Moise DIARRA <moisepr@gmail.com>
2024-10-04 14:47:31 +0000

    Add List_display

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
+from random import choice
+
 from django.db import models
 
 # Create your models here.
+CATEGORY_CHOICES=(
+    ('CR','Curd'),
+    ('ML', 'Milk' ),
+    ('LS', 'Lassi' ),
+    ('MS', 'Milkshake', ),
+    ('PN', 'Paneer' ),
+    ('GH', 'Ghee' ),
+    ('CZ', 'Cheese' ),
+    ('IC', 'Ice-Creams' ),
+)
+
+class   Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.CharField(default='', max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=100)
+    product_image = models.ImageField(upload_to='product/')
+
+    def __str__(self):
+        return self.title
